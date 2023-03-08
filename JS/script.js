c397dde23fb046f3c50f3c90a3001b97fc4e3369
usuario <laureano.bentancor@gmail.com>
2023-03-08 10:07:36 -0300

    Targetas listas ccs mejorado eliminacion de description falta botones

--- JS/script.js
+++ JS/script.js
@@ -6,19 +6,28 @@ async function getData(){
     const res  = await  fetch('https://fakestoreapi.com/products')
     const datos = await res.json();
     datos.forEach(dato=>{
-        
+       
+        console.log(datos);
+
 	const card = document.createElement('div');
     card.className = 'card';
+    
+    const title = document.createElement('h2');
+    title.className = 'title';
+    title.textContent = dato.title;
+    
     const img = document.createElement('img');
     img.src = dato. image
-    const desc = document.createElement('div');
-    desc.textContent = dato.description
-    desc.className = 'desc';
-    const price = document.createElement('div');
+
+    //const desc = document.createElement('div');
+    //desc.textContent = dato.description
+    //desc.className = 'desc';
+    
+    const price = document.createElement('h3');
     price.textContent = dato.price;
     price.className = price;
 
-    card.append(img, desc, price);
+    card.append( img, title, price);
     main.appendChild(card);
 	
     });
